1d4cb73b1be39f3e731915b7faa810b7d830a7c4
Leandro Toledo <leandro.toledo@estudiantes.utec.edu.uy>
2020-05-31 21:31:01 -0300

    Commit inicial del proyecto cliente Desktop (Frontend)

---
+++ GEONat_cliDesktop/appClientModule/Interfaz/FramePrincipal.java
@@ -0,0 +1,284 @@
+package Interfaz;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.Dimension;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.TitledBorder;
+import javax.swing.UIManager;
+import java.awt.Color;
+import javax.swing.JLabel;
+//import com.jgoodies.forms.factories.DefaultComponentFactory;
+
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class FramePrincipal {
+
+	JFrame frmGeonat;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FramePrincipal window = new FramePrincipal();
+					window.frmGeonat.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public FramePrincipal() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmGeonat = new JFrame();
+		frmGeonat.setMinimumSize(new Dimension(800, 600));
+		frmGeonat.setPreferredSize(new Dimension(800, 600));
+		frmGeonat.setResizable(false);
+		frmGeonat.setSize(new Dimension(800, 600));
+		frmGeonat.setTitle("GEONat");
+		frmGeonat.setBounds(100, 100, 552, 361);
+		frmGeonat.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmGeonat.getContentPane().setLayout(null);
+		
+		JPanel panelUsuarios = new JPanel();
+		panelUsuarios.setBorder(new TitledBorder(null, "Gesti\u00F3n de Usuarios", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelUsuarios.setBounds(10, 297, 220, 263);
+		frmGeonat.getContentPane().add(panelUsuarios);
+		panelUsuarios.setLayout(null);
+		
+		JButton btnUsuariosRegistrar = new JButton("Registrar");
+		btnUsuariosRegistrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							AltaUsuario window = new AltaUsuario();
+							window.frmGeonatAlta.setVisible(true);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		});
+		btnUsuariosRegistrar.setBounds(10, 32, 199, 23);
+		panelUsuarios.add(btnUsuariosRegistrar);
+		
+		JButton btnUsuariosVerListado = new JButton("Ver listado / Dar de Baja");
+		btnUsuariosVerListado.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							FrameListarUsuarios window = new FrameListarUsuarios();
+							window.frmListarUsuarios.setVisible(true);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		});
+		btnUsuariosVerListado.setBounds(10, 100, 199, 23);
+		panelUsuarios.add(btnUsuariosVerListado);
+		
+		JButton button = new JButton("Modificar");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							ModificacionUsuario window = new ModificacionUsuario();
+							window.frmModificarUsuario.setVisible(true);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		});
+		button.setBounds(10, 66, 199, 23);
+		panelUsuarios.add(button);
+		
+		JPanel panel_TiposDeUsuario = new JPanel();
+		panel_TiposDeUsuario.setLayout(null);
+		panel_TiposDeUsuario.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Tipos de Usuario", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_TiposDeUsuario.setBounds(10, 134, 200, 125);
+		panelUsuarios.add(panel_TiposDeUsuario);
+		
+		JButton button_7 = new JButton("Registrar");
+		button_7.setBounds(10, 23, 157, 23);
+		panel_TiposDeUsuario.add(button_7);
+		
+		JButton btnEliminar = new JButton("Eliminar");
+		btnEliminar.setBounds(10, 91, 157, 23);
+		panel_TiposDeUsuario.add(btnEliminar);
+		
+		JButton button_9 = new JButton("Modificar");
+		button_9.setBounds(10, 57, 157, 23);
+		panel_TiposDeUsuario.add(button_9);
+		
+		JPanel panelObservaciones = new JPanel();
+		panelObservaciones.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Gesti\u00F3n de Observaciones", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panelObservaciones.setBounds(10, 132, 774, 154);
+		frmGeonat.getContentPane().add(panelObservaciones);
+		panelObservaciones.setLayout(null);
+		
+		JButton btnObservacionesRegistrar = new JButton("Registrar");
+		btnObservacionesRegistrar.setEnabled(false);
+		btnObservacionesRegistrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+				
+				
+				
+			}
+		});
+		btnObservacionesRegistrar.setBounds(10, 31, 199, 23);
+		panelObservaciones.add(btnObservacionesRegistrar);
+		
+		JButton btnObservacionesVerListado = new JButton("Ver listado");
+		btnObservacionesVerListado.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+								
+			}
+			
+		});
+		btnObservacionesVerListado.setBounds(10, 99, 199, 23);
+		panelObservaciones.add(btnObservacionesVerListado);
+		
+		JButton btnObservacionesModificar = new JButton("Modificar");
+		btnObservacionesModificar.setEnabled(false);
+		btnObservacionesModificar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btnObservacionesModificar.setBounds(10, 65, 199, 23);
+		panelObservaciones.add(btnObservacionesModificar);
+		
+		JLabel lblTotalDeObservaciones = new JLabel("Total de observaciones Registradas:");
+		lblTotalDeObservaciones.setBounds(307, 35, 246, 14);
+		panelObservaciones.add(lblTotalDeObservaciones);
+		
+		JLabel label_1 = new JLabel("?");
+		label_1.setBounds(553, 35, 46, 14);
+		panelObservaciones.add(label_1);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(10, 11, 774, 100);
+		frmGeonat.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel img_logo = new JLabel("");
+		img_logo.setHorizontalAlignment(SwingConstants.CENTER);
+		img_logo.setIcon(new ImageIcon(FramePrincipal.class.getResource("/logo.png")));
+		img_logo.setBounds(274, 11, 225, 78);
+		panel.add(img_logo);
+		
+		JLabel lblRegistroDeObservaciones = new JLabel("Registro de observaciones medioambientales");
+		lblRegistroDeObservaciones.setForeground(Color.DARK_GRAY);
+		lblRegistroDeObservaciones.setHorizontalAlignment(SwingConstants.CENTER);
+		lblRegistroDeObservaciones.setBounds(10, 75, 754, 25);
+		panel.add(lblRegistroDeObservaciones);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setLayout(null);
+		panel_1.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Gesti\u00F3n de Caracter\u00EDsticas", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_1.setBounds(564, 297, 220, 193);
+		frmGeonat.getContentPane().add(panel_1);
+		
+		JButton button_1 = new JButton("Registrar");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							AltaCaracteristica window = new AltaCaracteristica();
+							window.frmG.setVisible(true);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+				
+			}
+		});
+		button_1.setBounds(10, 31, 199, 23);
+		panel_1.add(button_1);
+		
+		JButton btnVerListado = new JButton("Ver listado / Dar de Baja");
+		btnVerListado.setBounds(10, 99, 199, 23);
+		panel_1.add(btnVerListado);
+		
+		JButton button_3 = new JButton("Modificar");
+		button_3.setBounds(10, 65, 199, 23);
+		panel_1.add(button_3);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setLayout(null);
+		panel_2.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Gesti\u00F3n de Fen\u00F3menos", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panel_2.setBounds(289, 297, 220, 263);
+		frmGeonat.getContentPane().add(panel_2);
+		
+		JButton button_4 = new JButton("Registrar");
+		button_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							AltaFenomeno window = new AltaFenomeno();
+							window.frmAltaFenomenos.setVisible(true);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		});
+		button_4.setBounds(10, 31, 199, 23);
+		panel_2.add(button_4);
+		
+		JButton button_5 = new JButton("Ver listado");
+		button_5.setBounds(10, 99, 199, 23);
+		panel_2.add(button_5);
+		
+		JButton button_6 = new JButton("Modificar");
+		button_6.setBounds(10, 65, 199, 23);
+		panel_2.add(button_6);
+		
+		JButton btnCrearDatosPrueba = new JButton("Crear datos de prueba");
+		btnCrearDatosPrueba.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				DatosDePrueba.getInstance();
+			}
+		});
+		btnCrearDatosPrueba.setBounds(582, 537, 181, 23);
+		frmGeonat.getContentPane().add(btnCrearDatosPrueba);
+	}
+}
---
+++ GEONat_cliDesktop/appClientModule/Interfaz/ModificacionCaracteristica.java
@@ -0,0 +1,286 @@
+package Interfaz;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+//import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+import com.entities.Caracteristica;
+import com.entities.Fenomeno;
+import com.entities.TipoDato;
+import com.entities.TipoDocumento;
+import com.entities.TipoUsuario;
+import com.entities.Usuario;
+import com.exception.ServiciosException;
+import com.servicios.CaracteristicasBeanRemote;
+import com.servicios.FenomenosBean;
+import com.servicios.FenomenosBeanRemote;
+import com.servicios.UsuariosBeanRemote;
+
+import javax.swing.JComboBox;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.swing.JButton;
+
+public class ModificacionCaracteristica {
+
+	JFrame frmG;
+	private JTextField txtFNombre;
+	private JTextField txtFEtiqueta;
+	private List<Fenomeno> fenomenos;
+	private JTextField txtID;
+	
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				
+				
+				
+				try {
+					ModificacionCaracteristica window = new ModificacionCaracteristica();
+					window.frmG.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ModificacionCaracteristica() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmG = new JFrame();
+		frmG.setTitle("GEONat - Modificacion de una caracteristica");
+		frmG.setBounds(100, 100, 600, 300);
+		frmG.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmG.getContentPane().setLayout(null);
+		
+		JLabel lblNombre = new JLabel("Nombre de la caracter\u00EDstica");
+		lblNombre.setBounds(10, 66, 141, 14);
+		frmG.getContentPane().add(lblNombre);
+		
+		txtFNombre = new JTextField();
+		txtFNombre.setBounds(181, 63, 206, 20);
+		frmG.getContentPane().add(txtFNombre);
+		txtFNombre.setColumns(10);
+		
+		JLabel lblEtiqueta = new JLabel("Etiqueta de presentaci\u00F3n");
+		lblEtiqueta.setBounds(10, 104, 141, 14);
+		frmG.getContentPane().add(lblEtiqueta);
+		
+		txtFEtiqueta = new JTextField();
+		txtFEtiqueta.setBounds(181, 101, 206, 34);
+		frmG.getContentPane().add(txtFEtiqueta);
+		txtFEtiqueta.setColumns(10);
+		
+		JLabel lblTipoDeDato = new JLabel("Tipo de Dato");
+		lblTipoDeDato.setBounds(10, 158, 125, 14);
+		frmG.getContentPane().add(lblTipoDeDato);
+		
+		JLabel lblFenomenoAsociado = new JLabel("Fen\u00F3meno Asociado");
+		lblFenomenoAsociado.setBounds(10, 198, 125, 14);
+		frmG.getContentPane().add(lblFenomenoAsociado);
+		
+		
+// Combo Fenomeno Asociado		
+		JComboBox comboBFenomAsoc = new JComboBox();
+		comboBFenomAsoc.setBounds(181, 195, 206, 20);
+		frmG.getContentPane().add(comboBFenomAsoc);
+		
+		try {
+			fenomenos = cargarfenomenos();
+			for (Fenomeno f : fenomenos) {
+				comboBFenomAsoc.addItem(f.getNombre());
+			}
+		} catch (ServiciosException e1) {
+
+			e1.printStackTrace();
+		}
+		
+		
+		
+		comboBFenomAsoc.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent eventocombo) {
+				comboBFenomAsoc.getSelectedIndex();
+				
+			}
+		});	
+		
+		
+// Combo TipoDato
+		JComboBox comboBTipoDato = new JComboBox();
+		comboBTipoDato.setBounds(181, 155, 206, 20);
+		frmG.getContentPane().add(comboBTipoDato);
+		comboBTipoDato.removeAllItems(); 
+		comboBTipoDato.addItem("");
+		TipoDato[] tipoDatoList = TipoDato.values();
+		for (TipoDato tipoDato : tipoDatoList) {
+		comboBTipoDato.addItem(tipoDato);
+		}
+		comboBTipoDato.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent eventocombo) {
+				comboBTipoDato.getSelectedIndex();
+				
+			}
+		});
+		
+		JButton btnCancelarAlta = new JButton("Cancelar Modificacion");
+		btnCancelarAlta.setBounds(424, 153, 150, 23);
+		frmG.getContentPane().add(btnCancelarAlta);
+		btnCancelarAlta.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evento) {
+				frmG.hide();
+			}
+			
+		});
+		
+		
+		JButton btnDarDeAlta = new JButton("Confirmar Modificacion");
+		btnDarDeAlta.setBounds(424, 192, 150, 23);
+		frmG.getContentPane().add(btnDarDeAlta);
+		
+		JButton btnBuscaCaracteristica = new JButton("Busca Caracteristica");
+		btnBuscaCaracteristica.setBounds(424, 21, 150, 23);
+		frmG.getContentPane().add(btnBuscaCaracteristica);
+		txtID = new JTextField();
+		txtID.setBounds(181, 22, 206, 20);
+		frmG.getContentPane().add(txtID);
+		txtID.setColumns(10);
+		
+		JLabel lblIdCaracteritica = new JLabel("ID Caracteritica");
+		lblIdCaracteritica.setBounds(10, 25, 141, 14);
+		frmG.getContentPane().add(lblIdCaracteritica);
+
+		btnDarDeAlta.addActionListener(new ActionListener() {
+			
+		public void actionPerformed(ActionEvent evento) {
+			String strerror = "";
+			Boolean errores = false;
+			Caracteristica caract = new Caracteristica();
+			
+			
+			
+			
+			if ((txtFNombre.getText().length())>50) {
+				errores=true;
+				strerror= strerror + " Nombre caracteristica con mas de 50 caracteres. ";
+			} else {
+				caract.setNombre(txtFNombre.getText());
+			}
+			if ((txtFEtiqueta.getText().length())>50) {
+				errores=true;
+				strerror= strerror + " Etiqueta con mas de 50 caracteres. ";
+			} else {
+				caract.setEtiqPresentacion(txtFEtiqueta.getText());
+			}
+			
+			TipoDato td=(TipoDato.valueOf(comboBTipoDato.getSelectedItem().toString()) );
+//			caract.setId_TipoDato(td);
+			
+			Fenomeno fenomAlta = new Fenomeno();
+			for (Fenomeno f : fenomenos) {
+				if (f.getNombre()==comboBFenomAsoc.getSelectedItem()) {
+					fenomAlta.setId_Fenomeno(f.getId_Fenomeno());
+					fenomAlta.setNombre(f.getNombre());
+					fenomAlta.setDescripcion(f.getDescripcion());
+					fenomAlta.setTelefono(f.getTelefono());
+					
+				}
+			}
+//			caract.setId_Fenomeno(fenomAlta);
+			
+			if (errores) {
+				JOptionPane.showMessageDialog(null,  strerror);
+			} else {
+				try {
+					CaracteristicasBeanRemote CaracteristicasBean;
+					CaracteristicasBean = (CaracteristicasBeanRemote) InitialContext.doLookup("/GEONat/CaracteristicasBean!com.servicios.CaracteristicasBeanRemote");
+					CaracteristicasBean.create(caract);
+					JOptionPane.showMessageDialog(null,  "Caracteristica Creada");
+					txtFNombre.setText("");
+					txtFEtiqueta.setText("");
+					comboBFenomAsoc.setSelectedIndex(0);
+					comboBTipoDato.setSelectedIndex(0);
+					
+					} catch (NamingException | ServiciosException e) {
+						e.printStackTrace();
+						}
+				
+				
+				
+			}
+
+
+		}
+		});
+		
+		btnBuscaCaracteristica.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evento) {
+				Caracteristica c = new Caracteristica();
+				int id=Integer.parseInt(txtID.getText());
+				CaracteristicasBeanRemote CaracteristicasBean=null;
+				try {
+					CaracteristicasBean = (CaracteristicasBeanRemote) InitialContext.doLookup("/GEONat/CaracteristicasBean!com.servicios.CaraacteristicaBeanRemote");
+					boolean enu = CaracteristicasBean.existeIdCaracteristica(c);
+					if (!enu) {
+						JOptionPane.showMessageDialog(null,  "No existe la caracteristica");
+					} else {
+//						Caracteristica cc = new Caracteristica();
+//						cc = CaracteristicasBean.obtenerUno(c);
+//						txtID.setText(c.getId_Caracteristica().toString());
+//						cc.setNombre(txtFNombre.getText());
+//						cc.setEtiqPresentacion(txtFEtiqueta.getText());
+//						int x = cc.getId_TipoFonomeno();
+//						comboBFenomAsoc.setSelectedIndex(cc.getId_Fenomeno().getId_TipoFenomeno());
+//						comboBTipoDato.setSelectedIndex(cc.getId_TipoDato().getTipoDato());
+
+							
+					}
+				} catch (NamingException e) {
+					e.printStackTrace();
+				} catch (ServiciosException e) {
+					e.printStackTrace();
+				}
+			}
+			});
+	}
+	
+	
+	
+	
+	private static List<Fenomeno> cargarfenomenos() throws ServiciosException {
+		ArrayList<Fenomeno> fenomenos = null;
+		try {
+			FenomenosBeanRemote FenomenosBean;
+			FenomenosBean = (FenomenosBeanRemote) InitialContext.doLookup("/GEONat/FenomenosBean!com.servicios.FenomenosBeanRemote");
+			fenomenos =  (ArrayList<Fenomeno>) FenomenosBean.obtenerTodos(); 
+			
+			} catch (NamingException | ServiciosException e) {
+				e.printStackTrace();
+				}
+		
+		return fenomenos;
+	
+	
+	}
+	}
