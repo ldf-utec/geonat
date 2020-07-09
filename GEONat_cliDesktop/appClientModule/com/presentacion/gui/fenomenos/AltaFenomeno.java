671fe9302c89bbc3ffae702c6545dc838ee0984c
Karen Lima <karen.lima@estudiantes.utec.edu.uy>
2020-07-09 01:37:26 -0300

    Se cambio el lookup de alta de fenomeno

--- GEONat_cliDesktop/appClientModule/com/presentacion/gui/fenomenos/AltaFenomeno.java
+++ GEONat_cliDesktop/appClientModule/com/presentacion/gui/fenomenos/AltaFenomeno.java
@@ -145,7 +145,7 @@ public class AltaFenomeno {
 				fen.setNombre(txtFNombre.getText().toUpperCase());
 				try {
 					fenBean = (IFenomenoDAO) 
-							InitialContext.doLookup("GEONat/FenomenosDAO!com.DAO.FenomenoDAO11");
+							InitialContext.doLookup("GEONat/FenomenosDAO!com.DAO.interfaces.IFenomenoDAO");
 					boolean enu = fenBean.existeNombreFenomeno(fen);
 					
 					if (enu) {
