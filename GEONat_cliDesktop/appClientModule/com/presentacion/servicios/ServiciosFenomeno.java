1a408c37d6af6d87fba5cf86f8b09fae7f81fb2b
Leandro Toledo <leandro.toledo@estudiantes.utec.edu.uy>
2020-07-11 22:06:41 -0300

    se modifica ServicioFenomeno para que coincida con el DAO

--- GEONat_cliDesktop/appClientModule/com/presentacion/servicios/ServiciosFenomeno.java
+++ GEONat_cliDesktop/appClientModule/com/presentacion/servicios/ServiciosFenomeno.java
@@ -86,12 +86,10 @@ public class ServiciosFenomeno {
 	// TODO: Posiblemente este método no tiene mucho sentido, ya que al pasarse como parámetro el objeto Usuario, es más eficiente buscarlo por id.
 	// Si lo que se quiere es obtener por Nombre, se debería pasar el String nombre
 	// Obtener uno
-	public Fenomeno obtenerUno(Fenomeno fenomeno) throws ServiciosException {
-		
-		Integer filtro = fenomeno.getId_Fenomeno();
-		Fenomeno f = servicios.fenomenoBean.obtenerUno(filtro);
-		return f ;
-		
+	public Fenomeno obtenerUno(int id) throws ServiciosException {
+	
+		return servicios.fenomenoBean.obtenerUno(id); 
+	
 	  }
 	
 	public boolean existeNombreFenomeno(Fenomeno fenomeno) throws ServiciosException {
