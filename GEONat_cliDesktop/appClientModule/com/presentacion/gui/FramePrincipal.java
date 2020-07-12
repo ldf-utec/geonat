1618cf5181bb6cfa4732fd7da61a0b02723aefc6
Karen Lima <karen.lima@estudiantes.utec.edu.uy>
2020-07-11 22:24:20 -0300

    Se agregaron las ventanas de modificaci'on y listar fen'omeno

--- GEONat_cliDesktop/appClientModule/com/presentacion/gui/FramePrincipal.java
+++ GEONat_cliDesktop/appClientModule/com/presentacion/gui/FramePrincipal.java
@@ -9,6 +9,7 @@ import javax.swing.border.TitledBorder;
 
 import com.presentacion.gui.caracteristica.AltaCaracteristica;
 import com.presentacion.gui.fenomenos.AltaFenomeno;
+import com.presentacion.gui.fenomenos.FrameListarFenomenos;
 import com.presentacion.gui.fenomenos.ModificacionFenomeno;
 import com.presentacion.gui.usuarios.AltaUsuario;
 import com.presentacion.gui.usuarios.FrameListarUsuarios;
@@ -270,20 +271,20 @@ public class FramePrincipal {
 		panel_2.add(button_4);
 		
 		JButton button_5 = new JButton("Ver listado");
-//		button_5.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				EventQueue.invokeLater(new Runnable() {
-//					public void run() {
-//						try {
-//							AltaFenomeno window = new AltaFenomeno();
-//							window.frmAltaFenomenos.setVisible(true);
-//						} catch (Exception e) {
-//							e.printStackTrace();
-//						}
-//					}
-//				});
-//			}
-//		});
+		button_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				EventQueue.invokeLater(new Runnable() {
+					public void run() {
+						try {
+							FrameListarFenomenos window = new FrameListarFenomenos();
+							window.frmListarFenomenos.setVisible(true);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+			}
+		});
 		button_5.setBounds(10, 99, 199, 23);
 		panel_2.add(button_5);
 		
